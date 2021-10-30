16f778cfa1a5b9b9960b8631a650d7dada711bff
jivesha <89973127+jivesha@users.noreply.github.com>
2021-10-30 16:55:13 +0530

    Update SpeakGoodBye.js

--- module4_sol/SpeakGoodBye.js
+++ module4_sol/SpeakGoodBye.js
@@ -1,10 +1,11 @@
-
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
